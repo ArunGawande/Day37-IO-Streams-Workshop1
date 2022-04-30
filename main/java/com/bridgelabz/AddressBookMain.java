d57b4fbaf729c9c03b971e203fc2344af1605f60
Arun <arungawande1995@gmail.com>
2022-04-30 16:57:00 +0530

    Added: UC19 to retrive data city or state

--- main/java/com/bridgelabz/AddressBookMain.java
+++ main/java/com/bridgelabz/AddressBookMain.java
@@ -2,8 +2,7 @@ package com.bridgelabz;
 
 import java.time.LocalDate;
 import java.util.*;
-
-import static jdk.jfr.internal.consumer.EventLog.update;
+//import static jdk.jfr.internal.consumer.EventLog.update;
 
 public class AddressBookMain {
     static Scanner scanner = new Scanner(System.in);
@@ -13,8 +12,9 @@ public class AddressBookMain {
         boolean exit = false;
         while (!exit) {
             System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Update Address,city,state,zip  by srNo\n " +
-                    "3 Retrieve data for particular date" +
-                    "-> \n 4 -> exit");
+                    "3 -> Retrieve data for particular date\n" +
+                    "4 -> Retrieve Count of Contacts for City or State\n" +
+                    "5 -> exit");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
@@ -24,17 +24,58 @@ public class AddressBookMain {
                     update();
                     break;
                 case 3:
-                    reteriveDataForParticularDate();
+                    retrieveDataForParticularDate();
                     break;
                 case 4:
+                    retrieveCountByCityOrState();
+                case 5:
                     exit = true;
             }
         }
     }
 
-    private static void reteriveDataForParticularDate() {
+    private static void retrieveData() {
+        AddressBook addressBook = new AddressBook();
+        List<Contacts> employeeInfoList = addressBook.retrieveData();
+        for (Contacts employee : employeeInfoList
+        ) {
+            System.out.println(employee + "\n");
+        }
     }
 
-    private static void retrieveData() {
+    private static void update() {
+        AddressBook addressBook = new AddressBook();
+        System.out.println("Enter the address,city,state, zip and Serial Number  to Update");
+        addressBook.updateCityByZip(scanner.next(), scanner.next(), scanner.next(), scanner.nextInt(), scanner.nextInt());
+    }
+    private static void retrieveDataForParticularDate() {
+        AddressBook addressBook = new AddressBook();
+        System.out.println("Enter the Date of Joining (YYYY-MM-DD");
+        System.out.println("Enter year , month and Day ex: 2020 02 03");
+        List<Contacts> employeeInfoList = addressBook.findAllForParticularDate(LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+        for (Contacts employee : employeeInfoList
+        ) {
+            System.out.println(employee + "\n");
+        }
+    }
+    private static void retrieveCountByCityOrState() {
+        AddressBook addressBookRepo = new AddressBook();
+        System.out.println("Enter 1 -> Contacts count by City\n" +
+                "2 -> Contacts count by State");
+
+        switch (scanner.nextInt()) {
+            case 1:
+                System.out.println("Enter city Name");
+                int cityContactsCount = addressBookRepo.countByCiy(scanner.next());
+                System.out.println("Number of Contacts is Given city= " + cityContactsCount);
+                break;
+            case 2:
+                System.out.println("Enter state name");
+                int stateContactsCount=  addressBookRepo.countByState(scanner.next());
+                System.out.println("Number of Contacts is Given state= " + stateContactsCount);
+                break;
+        }
+
+
     }
 }
